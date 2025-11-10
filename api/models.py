c15f00a8edda0890b9c228a197fef356c255ea0c
djumanov <djumanovdev@gmail.com>
2025-11-10 17:31:58 +0500

    create products api

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    catogory = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField()
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    rating = models.IntegerField()
     active = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
